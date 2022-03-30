217e6a90c76dbc66870bd079691bb3c6a578a654
DaDvoy <lmushroo@student.21-school.ru>
2022-03-30 19:18:03 +0300

    start server

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int     main(int argc, char **argv) {
+    if (argc == 3) {
+
+    }
+    else {
+        std::cout << "Wrong arguments for launching\n";
+        return (1);
+    }
+    return (0);
+}
---
+++ src/Server.hpp
@@ -0,0 +1,18 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <iostream>
+#include <sys/socket.h>
+
+class Server
+{
+private:
+
+public:
+    Server();
+    ~Server();
+
+
+};
+
+#endif
