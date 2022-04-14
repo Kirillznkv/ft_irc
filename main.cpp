a73bcc23ae15baf4f7f51c71a19470dc87a66fb9
DaDvoy <anamia357@gmail.com>
2022-04-14 16:14:13 +0300

    update for school mac

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
+#include "Server.hpp"
 
 
 int     main(int argc, char **argv) {
     if (argc == 3) {
-
+        Server  server();
+        server.createConnection();
     }
     else {
-        std::cout << "Wrong arguments for launching\n";
+        std::cout << "Wrong arguments for launching:\n";
+        std::cout << "./ircserv <port> <password>\n";
         return (1);
     }
     return (0);
